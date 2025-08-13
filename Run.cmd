57ffa6461827a32b8a608e18b74c6e000d601a64
SalerSimo <salersimo@gmail.com>
2025-08-13 21:27:51 +0200

    Update Run.cmd

--- Run.cmd
+++ Run.cmd
@@ -1,3 +1,5 @@
+rmdir run /S /Q
+mkdir run
 copy "external\lib\SDL3.dll" "run\SDL3.dll"
-gcc src/*.c -Iinclude -lSDL3 -o run/RayTracing
+gcc src/*.c -Iinclude -lSDL3 -o run/RayTracing.exe
 .\run\RayTracing.exe
