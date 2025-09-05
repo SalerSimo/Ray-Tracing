731da9606f6b3ee80471ede59c63fce885b307d7
SalerSimo <salersimo@gmail.com>
2025-09-05 18:46:24 +0200

    Refactor management of hit points by using a dedicated Hit structure

--- src/raytracer.c
+++ src/raytracer.c
@@ -1,12 +1,19 @@
-#include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 #include"raytracer.h"
 
 #define SHADOW_SAMPLES 20
 
+typedef struct{
+	Point* point;
+	Vector normal;
+	Material material;
 
-Point *Model_intersection(Model *model, Ray *l, Triangle **p_t);
+	Model *model;
+}Hit;
+
+
+Hit Model_intersection(Model *model, Ray *l);
 Color TraceRayR(Scene *scene, Ray *l, int depth);
 
 Color TraceRay(Scene *scene, Ray *ray){
@@ -17,16 +24,15 @@ Vector Reflect(Vector incident, Vector normal) {
 	return Vector_sum(incident, Vector_scale(normal, -2 * Vector_dot(incident, normal)));
 }
 
-int isInShadow(Scene *scene, Model *nearModel, Point *intersectionPoint, Point *lightPoint){
-	Vector toLight =  Vector_fromPoints(intersectionPoint, lightPoint);
-	Ray *shadowRay = Line_init(intersectionPoint, toLight);
+int isInShadow(Scene *scene, Hit realHit, Point *lightPoint){
+	Vector toLight =  Vector_fromPoints(realHit.point, lightPoint);
+	Ray *shadowRay = Line_init(realHit.point, toLight);
 	for (int i = 0; i < scene->numModels; i++) {
-		if (scene->models[i] == nearModel || scene->models[i] == NULL || scene->models[i]->type == LIGHT) continue;
-		Triangle *t;
-		Point *p = Model_intersection(scene->models[i], shadowRay, &t);
-		if (p != NULL) {
-			double distToObj = Point_distanceSquared(p, intersectionPoint);
-			double distToLight = Point_distanceSquared(lightPoint, intersectionPoint);
+		if (scene->models[i] == realHit.model || scene->models[i] == NULL || scene->models[i]->type == LIGHT) continue;
+		Hit hit = Model_intersection(scene->models[i], shadowRay);
+		if (hit.point != NULL) {
+			double distToObj = Point_distanceSquared(hit.point, realHit.point);
+			double distToLight = Point_distanceSquared(lightPoint, hit.point);
 			if (distToObj < distToLight - 1e-5) {
 				return 1;
 			}
@@ -35,10 +41,10 @@ int isInShadow(Scene *scene, Model *nearModel, Point *intersectionPoint, Point *
 	return 0;
 }
 
-double CalculateShadowFactor(Scene *scene, Model *nearModel, Vector normal, Vector vectorLight, Point *intersectionPoint){
+double CalculateShadowFactor(Scene *scene, Hit realHit, Vector vectorLight){
 	double epsilon = 1e-3;
-	Vector offset = Vector_scale(normal, epsilon);
-	Point *rayOrigin = Point_translate(intersectionPoint, offset);
+	Vector offset = Vector_scale(realHit.normal, epsilon);
+	realHit.point = Point_translate(realHit.point, offset);
 	int inShadow = 0;
 	double shadowFactor = 1;
 	Light *light = scene->lightSource;
@@ -51,7 +57,7 @@ double CalculateShadowFactor(Scene *scene, Model *nearModel, Vector normal, Vect
 		double angle = 2 * M_PI / numCheck;
 		for(int i = 0; i < numCheck; i++){
 			e1 = Vector_rotate(e1, vectorLight, angle);
-			if(isInShadow(scene, nearModel, rayOrigin, Point_translate(light->position, e1))){
+			if(isInShadow(scene, realHit, Point_translate(light->position, e1))){
 				inShadow = 1;
 				break;
 			}
@@ -69,13 +75,13 @@ double CalculateShadowFactor(Scene *scene, Model *nearModel, Vector normal, Vect
 				
 				Point *randomLightPoint = Point_translate(light->position, randomTraslation);
 
-				occluded += isInShadow(scene, nearModel, rayOrigin, randomLightPoint);
+				occluded += isInShadow(scene, realHit, randomLightPoint);
 			}
 			shadowFactor = 1.0 - ((double)occluded / numSamples);
 		}
 	}
 	else{
-		shadowFactor = 1 - isInShadow(scene, nearModel, rayOrigin, light->position);
+		shadowFactor = 1 - isInShadow(scene, realHit, light->position);
 	}
 
 	shadowFactor = fmax(shadowFactor, 0.1);
@@ -83,83 +89,73 @@ double CalculateShadowFactor(Scene *scene, Model *nearModel, Vector normal, Vect
 }
 
 Color TraceRayR(Scene *scene, Ray *ray, int depth){
-	Triangle *t;
-	Model *nearModel = NULL;
 	double minDistance = -1;
-	Point *intersectionPoint = NULL;
-	Triangle *intersectionTriangle = NULL;
-	Point *lightPosition = scene->lightSource->position;
 
 	Light *light = scene->lightSource;
+	Hit realHit;
+	realHit.point = NULL;
 
 	for(int i = 0; i < scene->numModels; i++){
 		if(scene->models[i] == NULL) continue;
-		if(intersectionPoint != NULL && Vector_dot(Vector_fromPoints(scene->models[i]->center, intersectionPoint), ray->direction) < 0 && Point_distanceSquared(scene->models[i]->center, intersectionPoint) > scene->models[i]->maxDistanceFromCenter * scene->models[i]->maxDistanceFromCenter){
+		if(realHit.point != NULL && Vector_dot(Vector_fromPoints(scene->models[i]->center, realHit.point), ray->direction) < 0 && Point_distanceSquared(scene->models[i]->center, realHit.point) > scene->models[i]->maxDistanceFromCenter * scene->models[i]->maxDistanceFromCenter){
 			continue;
 		}
-		Point *p = Model_intersection(scene->models[i], ray, &t);
-		if(p != NULL){
-			double distance = Point_distanceSquared(ray->origin, p);
+		Hit currentHit = Model_intersection(scene->models[i], ray);
+		if(currentHit.point != NULL){
+			double distance = Point_distanceSquared(ray->origin, currentHit.point);
 			if(minDistance == -1 || distance < minDistance){
-				nearModel = scene->models[i];
-				intersectionPoint = p;
-				intersectionTriangle = t;
+				realHit = currentHit;
 				minDistance = distance;
 			}
 		}
 	}
-	if(nearModel == NULL) return Color_multiply(BACKGROUND_COLOR, light->color);
-	if (nearModel->type == LIGHT) return intersectionTriangle->color;
+	if(realHit.point == NULL) return Color_multiply(BACKGROUND_COLOR, light->color);
+	if (realHit.model->type == LIGHT) return realHit.material.color;
 
-	Vector vectorLight = Vector_normalize(Vector_fromPoints(intersectionPoint, lightPosition));
+	Vector vectorLight = Vector_normalize(Vector_fromPoints(realHit.point, light->position));
 
-	Vector normal;
-	if(nearModel->type == SPHERE)
-		normal = Vector_fromPoints(nearModel->center, intersectionPoint);
-	else
-		normal = Triangle_getNormal(intersectionTriangle);
-	normal = Vector_normalize(normal);
-	if(Vector_dot(normal, ray->direction) > 0)
-		normal = Vector_scale(normal, -1);
+	if(Vector_dot(realHit.normal, ray->direction) > 0)
+		realHit.normal = Vector_scale(realHit.normal, -1);
 
-	double shadowFactor = CalculateShadowFactor(scene, nearModel, normal, vectorLight, intersectionPoint);
+	realHit.normal = Vector_normalize(realHit.normal);
+
+	double shadowFactor = CalculateShadowFactor(scene, realHit, vectorLight);
 
 	Vector oppositeDirection = Vector_normalize(Vector_scale(ray->direction, -1));
-	Vector R;
 
-	double diffuseStrength = fmax(0.1, Vector_dot(normal, vectorLight));
+	double diffuseStrength = fmax(0.1, Vector_dot(realHit.normal, vectorLight));
 
-	Vector tempN = Vector_scale(normal, 2 * Vector_dot(normal, vectorLight));
-	R = Vector_normalize(Vector_sum(tempN, Vector_scale(vectorLight, -1)));
+	Vector tempN = Vector_scale(realHit.normal, 2 * Vector_dot(realHit.normal, vectorLight));
+	Vector R = Vector_normalize(Vector_sum(tempN, Vector_scale(vectorLight, -1)));
 
 	int shininess = 32;
 	double spec = pow(fmax(Vector_dot(R, oppositeDirection), 0.0), shininess);
-	double specularStrength = nearModel->shininess;
+	double specularStrength = realHit.material.shininess;
 	
-	Color diffuseColor = Color_scale(Color_multiply(intersectionTriangle->color, light->color), diffuseStrength * shadowFactor);
+	Color diffuseColor = Color_scale(Color_multiply(realHit.material.color, light->color), diffuseStrength * shadowFactor);
 	Color specularColor = Color_scale(COLOR_WHITE, specularStrength * spec * shadowFactor);
 
-	if (nearModel->reflexivity > 0 && depth < MAX_DEPTH) {
-		Vector reflex = Reflect(ray->direction, normal);
+	if (realHit.material.reflexivity > 0 && depth < MAX_DEPTH) {
+		Vector reflex = Reflect(ray->direction, realHit.normal);
 		double epsilon = 1e-5;
-		Vector delta = Vector_scale(normal, epsilon);
+		Vector delta = Vector_scale(realHit.normal, epsilon);
 
-		Ray *reflexRay = Line_init(Point_translate(intersectionPoint, delta), reflex);
+		Ray *reflexRay = Line_init(Point_translate(realHit.point, delta), reflex);
 		Color reflectedColor = TraceRayR(scene, reflexRay, depth + 1);
 		reflectedColor = Color_scale(reflectedColor, 0.95); // a model cannot reflect 100% of the light it absorbs
-		diffuseColor = Color_blend(diffuseColor, reflectedColor, nearModel->reflexivity);
+		diffuseColor = Color_blend(diffuseColor, reflectedColor, realHit.material.reflexivity);
 	}
 	Color finalColor = Color_add(diffuseColor, specularColor);
 
 
-	double distanceSquared = Point_distanceSquared(intersectionPoint, lightPosition);
+	double distanceSquared = Point_distanceSquared(realHit.point, light->position);
 	double attenuation = light->constant + light->linear * sqrt(distanceSquared) + light->quadratic * distanceSquared;
 	attenuation = 1 / attenuation;
 
 	return Color_scale(finalColor, attenuation);
 }
 
-Point *intersectionPoint(Ray *ray, Triangle *t){
+Point *intersection_Point(Ray *ray, Triangle *t){
 	Point *A, *B, *C;
 	A = t->a;
 	B = t->b;
@@ -196,11 +192,13 @@ Point *intersectionPoint(Ray *ray, Triangle *t){
 	return intersectionPoint;
 }
 
-Point *Sphere_intersection(Model *sphere, Ray *ray) {
+Hit Sphere_intersection(Model *sphere, Ray *ray) {
+	Hit hit;
+	hit.point = NULL;
 	Point *O = ray->origin;
 	Vector D = ray->direction;
 	Point *C = sphere->center;
-	double r = sphere->maxDistanceFromCenter;
+	double r = fmax(0.1, sphere->maxDistanceFromCenter);
 
 	Vector L = Vector_fromPoints(C, O);
 
@@ -213,7 +211,7 @@ Point *Sphere_intersection(Model *sphere, Ray *ray) {
 
 	if (discriminant < 0) {
 		// No intersection
-		return NULL;
+		return hit;
 	}
 
 	double sqrt_discriminant = sqrt(discriminant);
@@ -223,43 +221,64 @@ Point *Sphere_intersection(Model *sphere, Ray *ray) {
 	double t;
 	if (t1 > 0) t = t1;
 	else if (t2 > 0) t = t2;
-	else return NULL; // Both intersections are behind the camera
+	else return hit; // Both intersections are behind the camera
 
 	// Compute intersection point: P = O + t * D
 	Point* intersection = Point_translate(O, Vector_scale(D, t));
 
-	return intersection;
+	hit.point = intersection;
+	hit.normal = Vector_fromPoints(sphere->center, intersection);
+	hit.model = sphere;
+	Material mat;
+	mat.color = sphere->triangles[0]->color;
+	mat.reflexivity = sphere->reflexivity;
+	mat.shininess = sphere->shininess;
+	hit.material = mat;
+
+	return hit;
 }
 
-Point *Model_intersection(Model *model, Ray *ray, Triangle **p_t){
-	*p_t = NULL;
+Hit Model_intersection(Model *model, Ray *ray){
 	if(model->type == SPHERE || model->type == LIGHT){
-		*p_t = model->triangles[0];
 		return Sphere_intersection(model, ray);
 	}
+	Hit hit;
+	hit.point = NULL;
 	double linePointDistance = Line_Point_distance(ray, model->center);
 	if(linePointDistance > model->maxDistanceFromCenter){
-		return NULL;
+		return hit;
 	}
 
 	Point *q = Line_projectionPoint(ray, model->center);
 	Vector pq = Vector_fromPoints(ray->origin, q);
 	if(Vector_dot(pq, ray->direction) + model->maxDistanceFromCenter < 0){
-		return NULL;
+		return hit;
 	}
 
 	Point *intersection = NULL;
 	double minDistance = -1;
+	Triangle *hitTriangle = NULL;
 	for(int i = 0; i < model->numTriangles; i++){
-		Point *p = intersectionPoint(ray, model->triangles[i]);
+		Point *p = intersection_Point(ray, model->triangles[i]);
 		if(p != NULL){
 			double distance = Point_distanceSquared(ray->origin, p);
 			if(minDistance == -1 || distance < minDistance){
 				intersection = p;
-				*p_t = model->triangles[i];
+				hitTriangle = model->triangles[i];
 				minDistance = distance;
 			}
 		}
 	}
-	return intersection;
+
+	if(intersection == NULL) return hit;
+
+	hit.point = intersection;
+	hit.normal = Triangle_getNormal(hitTriangle);
+	hit.model = model;
+	Material mat;
+	mat.color = (hitTriangle)->color;
+	mat.reflexivity = model->reflexivity;
+	mat.shininess = model->shininess;
+	hit.material = mat;
+	return hit;
 }
